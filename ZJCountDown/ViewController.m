b98d10b39054d25c24cc954fc12bb948201436f2
猪猪行天下 <873391579@qq.com>
2018-01-17 14:24:24 +0800

    添加效果图

--- ZJCountDown/ViewController.m
+++ ZJCountDown/ViewController.m
@@ -22,7 +22,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     _countDownView.cornerRadius = 10.0;
     _countDownView.textFont = [UIFont boldSystemFontOfSize:20];
-    [_countDownView countDownWithTimeInterval:5 completion:^(BOOL finished) {
+    [_countDownView countDownWithTimeInterval:15 completion:^(BOOL finished) {
         NextViewController *vc = [NextViewController new];
         
         [self.navigationController pushViewController:vc animated:YES];
